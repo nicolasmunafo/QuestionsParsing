e7d961db6ee14bb4f07108c31f02ba084766c921
nicolasmunafo <nicolas.a.munafo@gmail.com>
2026-06-16 18:05:33 +0200

    Updated TODOs

--- main.py
+++ main.py
@@ -12,11 +12,8 @@ def main():
 if __name__ == "__main__":
     main()
 
-# TODO: Store latest directory where it was opened
+# TODO: Show the latest directory in the label
 # TODO: Align buttons and text in the window
 # TODO: What if changes back directory? it stacks in the label
 # TODO: Enable button to create file only when both directories are set
 # TODO: PyMuPDF (fitz) to get Bold and format
-
-# DONE
-# TODO: What if file is already created? OK: 
